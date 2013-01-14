30a46562420f41f4ba4cb947b0b6f039ef22f9f5
lmachado <lmachado@inf.ufsm.br>
2013-01-14 13:53:30 +0000

    versao1

--- simulador/src/Questoes.java
+++ simulador/src/Questoes.java
@@ -1,11 +1,40 @@
-/*
- * @author NaysingerI
- */
+import javax.swing.JOptionPane;
+
 public class Questoes {
     private String titulo;
     private String descricao;
     private String[] alternativas = new String[5];
+    private String fonte;
+    private int resposta;
+    private int i;
     
+    public Questoes(){
+        this.titulo = JOptionPane.showInputDialog("titulo");
+        this.descricao = JOptionPane.showInputDialog("descricao");
+        for(i = 0; i < 5; i++){
+            this.alternativas[i] = JOptionPane.showInputDialog("Insira a alternativa " + i+1);
+        }
+        this.fonte = JOptionPane.showInputDialog("fonte");
+        this.resposta = Integer.parseInt(JOptionPane.showInputDialog("Resposta"));
+    }
     
+    public String getTitulo(){
+        return titulo;
+    }
     
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public String[] getAlternativas(){
+        return alternativas;
+    }
+    
+    public String getFonte(){
+        return fonte;
+    }
+    
+    public int getResposta(){
+        return resposta;
+    }
 }
