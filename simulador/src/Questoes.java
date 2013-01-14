29b339bc359fe3dcb48393cd7eb0e410d415ede5
lmachado <lmachado@inf.ufsm.br>
2013-01-14 13:50:11 +0000

    trabalho paradigmas

---
+++ simulador/src/Questoes.java
@@ -0,0 +1,11 @@
+/*
+ * @author NaysingerI
+ */
+public class Questoes {
+    private String titulo;
+    private String descricao;
+    private String[] alternativas = new String[5];
+    
+    
+    
+}
